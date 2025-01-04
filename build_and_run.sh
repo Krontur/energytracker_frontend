4e6b94f062e065929316ee4451cbfdf881fe747d
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2025-01-04 17:41:42 +0100

    Refactored script to dockerize frontend: file build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -e  # Habilita el modo "exit on error" para salir si algún comando falla
+set -e
+
+echo "Installing dependencies for frontend..."
+pnpm install || { echo "Failed to install dependencies. Exiting..."; exit 1; }
 
 echo "Starting build process for frontend..."
-
-# Ejecutar el comando para construir el frontend usando pnpm
 pnpm run build || { echo "Build process failed. Exiting..."; exit 1; }
 
 echo "Build process completed successfully!"
-
-# Levantar los contenedores con Docker Compose
 echo "Starting Docker Compose..."
 docker-compose up --build || { echo "Docker Compose failed to start. Exiting..."; exit 1; }
 

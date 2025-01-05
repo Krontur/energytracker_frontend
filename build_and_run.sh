30853e8bb78bb659d6de3bc3ed9843965e352c7a
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2025-01-05 12:10:26 +0100

    Add detached mode (-d) to Docker Compose in build script to prevent terminal blocking

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,6 +10,6 @@ pnpm run build || { echo "Build process failed. Exiting..."; exit 1; }
 
 echo "Build process completed successfully!"
 echo "Starting Docker Compose..."
-docker-compose up --build || { echo "Docker Compose failed to start. Exiting..."; exit 1; }
+docker-compose up --build -d || { echo "Docker Compose failed to start. Exiting..."; exit 1; }
 
 echo "Docker Compose started successfully!"
